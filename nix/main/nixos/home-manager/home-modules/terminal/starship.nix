50079e35524a0959f83ea6b97bbc275b265e722f
kumoren <mnievas640@gmail.com>
2026-08-16 19:26:00 -0300

    Changed starship

--- nix/main/nixos/home-manager/home-modules/terminal/starship.nix
+++ nix/main/nixos/home-manager/home-modules/terminal/starship.nix
@@ -9,14 +9,16 @@
       add_newline = true;
 
       format = lib.concatStrings [
+        " $hostname/"
+        "$username"
+        # "$line_break"
         "$directory"
+        "$fill"
         "$git_branch"
         "$git_status"
-        "$fill"
         "$nix_shell"
-        "$time"
         "$line_break"
-        "$character"
+        " ($character)  "
       ];
 
       fill = {
@@ -31,9 +33,30 @@
       };
 
       character = {
-        success_symbol = "";
-        error_symbol = "";
+        success_symbol = "";
+        error_symbol = "";
         vicmd_symbol = "󱢖";
+      };
+
+      username = {
+        disabled = false;
+        format = "[$user]($style)";
+        style_user = "bold blue";
+        show_always = true;
+      };
+
+      hostname = {
+        disabled = false;
+        format = "[$hostname]($style)";
+        style = "bold red";
+        ssh_only = false;
+      };
+
+      directory = {
+        disabled = false;
+        format = " -> [$path]($style)";
+        style = "bold cyan";
+        truncation_length = 2;
       };
 
       time = {
